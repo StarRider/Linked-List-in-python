819c34f7e771e9f31990f64fa9b11f99d2e7ef1a
StarRider <sharonalexander2596@gmail.com>
2018-08-06 08:09:39 +0530

    Add files via upload
    My First code on github

---
+++ linkedList.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  6 07:24:57 2018
+
+@author: SHARON T ALEXANDER
+"""
+
+class Node:
+    
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    
+    def __init__(self):
+        self.head = None
+    
+    def push(self,data):
+        newNode = Node(data)
+        newNode.next = self.head
+        self.head = newNode
+        
+    def pop(self):
+        if(self.head != None):
+            self.head = self.head.next
+        else:
+            print('No More Elements!')
+    
+    def show(self):
+        temp = self.head
+        print(end = '\n')
+        while(temp):
+            print(temp.data,end=' ')
+            temp = temp.next
+
+#main
+myList = LinkedList()
+num = list(map(int,input().split()))
+
+for i in num:
+    myList.push(i)
+    
+myList.show()
+choice = 1
+while(choice == 1):    
+    myList.pop()
+    myList.show()
+    choice = int(input())
+
+
+
+            
+        
