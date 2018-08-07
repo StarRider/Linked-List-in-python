1522517fe2285e0b9dd2e42feb2a448f3803d00f
StarRider <sharonalexander2596@gmail.com>
2018-08-07 19:45:03 +0530

    Update linkedList.py

--- linkedList.py
+++ linkedList.py
@@ -3,7 +3,6 @@
 Created on Mon Aug  6 07:24:57 2018
 
 @author: SHARON T ALEXANDER
-new change
 """
 
 class Node:
@@ -34,22 +33,52 @@ class LinkedList:
         while(temp):
             print(temp.data,end=' ')
             temp = temp.next
-
+    
+    def reverse(self):
+        prev = None
+        cur = self.head
+        while(cur is not None):
+            nex = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nex
+        self.head = prev
+        return self.head
+    
+    def detectLoop(self):
+        slow = self.head
+        fast = self.head
+        while(slow and fast and fast.next):
+            slow = slow.next
+            fast = fast.next.next
+            if(slow == fast):
+                print('Found Loop')
+                return
+            
+        
 #main
 myList = LinkedList()
+myRev = LinkedList()
 num = list(map(int,input().split()))
 
 for i in num:
     myList.push(i)
     
-myList.show()
-choice = 1
-while(choice == 1):    
-    myList.pop()
-    myList.show()
-    choice = int(input())
+#myList.show()
+#myList.reverse()
+#myRev.head = myList.show()
+#myNew = myList
+#myNew.show()
+#myRev.show()
+#choice = 1
+#while(choice == 1):    
+#    myList.pop()
+#    myList.show()
+#    choice = int(input())
 
+myList.head.next.next.next = myList.head
 
+myList.detectLoop()
 
             
         
